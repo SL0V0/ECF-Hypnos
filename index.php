7aa4871fa80daa496b140785632bc5a2ce5513c3
SL0V0 <samleboss2@gmail.com>
2022-03-18 14:43:09 +0100

    v1 de l'index avec le css et les images

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <title>HYPNOS</title>
+</head>
+<body>
+    <header>
+        <a href="index.php"><img class="imgLogo" src="images/logo.png" alt="logo"></a>
+        <nav>
+            <span class="spanNav"><h2 class="h2Nav"><a class="aNav" href="#">Réserver</a></h2></span>
+            <span class="spanNav"><h2 class="h2Nav"><a class="aNav" href="#">Nous contacter</a></h2></span>
+            <span class="spanNav"><h2 class="h2Nav"><a class="aNav" href="#">S'inscrire</a></h2></span>
+            <span class="spanNav"><h2 class="h2Nav"><a class="aNav" href="#">Se connecter</a></h2></span>
+        </nav>
+    </header>
+    <main>
+        <article class="articleMain">
+            <section class="sectionH1">
+                <h1>Choisissez une destination parmi nos établissements hôteliers</h1>
+            </section>
+            <section class="sectionArticleMain">
+                <div class="divChildVille"><a class="aSectionDestination" href="#">Paris</a></div>
+                <div class="divChildVille"><a class="aSectionDestination" href="#">Marseille</a></div>
+                <div class="divChildVille"><a class="aSectionDestination" href="#">Toulouse</a></div>
+                <div class="divChildVille"><a class="aSectionDestination" href="#">Metz</a></div>
+                <div class="divChildVille"><a class="aSectionDestination" href="#">Coustellet</a></div>
+                <div class="divChildVille"><a class="aSectionDestination" href="#">Lyon</a></div>
+                <div class="divChildVille"><a class="aSectionDestination" href="#">Rombas</a></div>
+            </section>
+        </article>
+    </main>
+    <footer>
+        <p class="pFooter">2004 - 2022 Hypnos.com. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
