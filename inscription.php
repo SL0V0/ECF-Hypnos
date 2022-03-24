5d74af9663f8936e9a436f126b7392b209e34490
SL0V0 <samleboss2@gmail.com>
2022-03-24 21:47:28 +0100

    V6 gestion des erreur du formulaire d'inscription

--- inscription.php
+++ inscription.php
@@ -16,15 +16,24 @@
             <h1>Inscription</h1>
             <div class="divLabInp">
                 <label for="nom">Nom</label>
-                <input type="text" name="nom" id="nom" >
+                <input type="text" name="nom" maxlength="40" id="nom" required>
+                <div class="divError">2 caractères minimums et 40 maximum</div>
+
                 <label for="prenom">Prénom</label>
-                <input type="text" name="prenom" id="prenom" required>
+                <input type="text" name="prenom" id="prenom" maxlength="40" required>
+                <div class="divError">2 caractères minimums et 40 maximum</div>
+
                 <label for="email">E-mail</label>
-                <input type="email" id="email" name="email" required>
+                <input type="email" id="email" name="email" maxlength="60" required>
+                <div class="divError">E-mail existant ou invalide</div>
+
                 <label for="mdp">Mot de passe</label>
-                <input type="password" name="mdp" id="mdp" required>
+                <input type="password" name="mdp" id="mdp" maxlength="60" required>
+                <div class="divError">6 caractères minimums et 60 caractères maximum</div>
+
                 <label for="confmdp">Confirmation mot de passe</label>
-                <input type="password" name="confmdp" id="confmdp" required>
+                <input type="password" name="confmdp" id="confmdp" maxlength="60" required>
+                <div class="divError">Ne correspond pas au mot de passe</div>
             </div>
             <div class="divBtnLink">
                 <input id="btnSubmit" type="submit" value="INSCRIPTION">
--- js/inscription.js
+++ js/inscription.js
@@ -1,34 +1,76 @@
 let btnSubmit = document.getElementById('btnSubmit')
-console.log('ah')
 
 btnSubmit.addEventListener('click', function(event)
 {
     event.preventDefault()
 
-    let nom = document.getElementById('nom').value
-    let prenom = document.getElementById('prenom').value
-    let email = document.getElementById('email').value
-    let password = document.getElementById('mdp').value
-    let confPass = document.getElementById('confmdp').value
+    let main = document.querySelector("main")
+    let divError = document.querySelectorAll('.divError')
+
+    let nom = document.getElementById('nom')
+    let prenom = document.getElementById('prenom')
+    let email = document.getElementById('email')
+    let password = document.getElementById('mdp')
+    let confPass = document.getElementById('confmdp')
 
     let inscription = {
 
-        nom: nom,
-        prenom: prenom,
-        email: email,
-        password: password,
-        confPass: confPass
+        nom: nom.value,
+        prenom: prenom.value,
+        email: email.value,
+        password: password.value,
+        confPass: confPass.value
     }
 
     
     console.log(inscription)
-
-    console.log(nom.length)
     
-    if(nom.length == 0 || prenom.length == 0 || email.length == 0 || password.length == 0 || confPass.length == 0)
+    if(nom.value.length == 0)
     {
+        nom.style.borderColor = "red"
+    }
+    else
+    {
+        nom.style.borderColor = "#bd9e56"
+    }
 
-    }else
+    if(prenom.value.length == 0)
+    {
+        prenom.style.borderColor = "red"
+    }
+    else
+    {
+        prenom.style.borderColor = "#bd9e56"
+    }
+
+    if(email.value.length == 0)
+    {
+        email.style.borderColor = "red"
+    }
+    else
+    {
+        email.style.borderColor = "#bd9e56"
+    }
+
+    if(password.value.length == 0)
+    {
+        password.style.borderColor = "red"
+    }
+    else
+    {
+        password.style.borderColor = "#bd9e56"
+    }
+
+    if(confPass.value.length == 0)
+    {
+        confPass.style.borderColor = "red"
+    }
+    else
+    {
+        confPass.style.borderColor = "#bd9e56"
+    }
+
+    if(nom.value.length !== 0 && prenom.value.length !== 0 && email.value.length !== 0 && password.value.length !== 0 && confPass.value.length !== 0)
     {
         $.ajax({
             url: "js/ajax/ajax_inscription.php",
@@ -38,6 +80,72 @@ btnSubmit.addEventListener('click', function(event)
 
                 let reponse = JSON.parse(res)
                 console.log(reponse)
+                console.log("--------------------------")
+                console.log(reponse.nom)
+
+                if(reponse.nom == 1)
+                {
+                    divError[0].style.visibility = "visible"
+                    nom.style.borderColor = "red"
+                }
+                else
+                {
+                    divError[0].style.visibility = "hidden"
+                    nom.style.borderColor = "#bd9e56"
+                }
+
+                if(reponse.prenom == 1)
+                {
+                    divError[1].style.visibility = "visible"
+                    prenom.style.borderColor = "red"
+                }
+                else
+                {
+                    divError[1].style.visibility = "hidden"
+                    prenom.style.borderColor = "#bd9e56"
+                }
+
+                if(reponse.email == 1)
+                {
+                    divError[2].style.visibility = "visible"
+                    email.style.borderColor = "red"
+                }
+                else
+                {
+                    divError[2].style.visibility = "hidden"
+                    email.style.borderColor = "#bd9e56"
+                }
+
+                if(reponse.mdp == 1)
+                {
+                    divError[3].style.visibility = "visible"
+                    password.style.borderColor = "red"
+                }
+                else
+                {
+                    divError[3].style.visibility = "hidden"
+                    password.style.borderColor = "#bd9e56"
+                }
+
+                if(reponse.confMdp == 1)
+                {
+                    divError[4].style.visibility = "visible"
+                    confPass.style.borderColor = "red"
+                }
+                else
+                {
+                    divError[4].style.visibility = "hidden"
+                    confPass.style.borderColor = "#bd9e56"
+                }
+                if(reponse.nom == 0 && reponse.prenom == 0 && reponse.email == 0 && reponse.mdp == 0 && reponse.confMdp == 0)
+                {
+                    main.innerHTML = `<h1>Incription validé</h1>`
+                    function redirection()
+                    {
+                        window.location.replace("connexion.php");
+                    }
+                    setTimeout(redirection, 3000)
+                }
                 
             }
         })
